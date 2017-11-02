0bca05b9d292005fd542d4b6a58d13525bf6f396
Benoit <bntdbs@gmail.com>
2017-11-02 19:45:06 +0100

    ajout fixtures

--- src/OC/UserBundle/DataFixtures/ORM/LoadUser.php
+++ src/OC/UserBundle/DataFixtures/ORM/LoadUser.php
@@ -9,29 +9,38 @@ use OC\UserBundle\Entity\User;
 
 class LoadUser implements FixtureInterface
 {
-  public function load(ObjectManager $manager)
-  {
-    // Les noms d'utilisateurs à créer
-    $listNames = array('Alexandre', 'Marine', 'Anna');
+    public function load(ObjectManager $manager)
+    {
+        // Les noms d'utilisateurs à créer
+        $listNames = array('Alexandre', 'Marine', 'Anna');
 
-    foreach ($listNames as $name) {
-      // On crée l'utilisateur
-      $user = new User;
+        foreach ($listNames as $name) {
+            // On crée l'utilisateur
+            $user = new User;
 
-      // Le nom d'utilisateur et le mot de passe sont identiques pour l'instant
-      $user->setUsername($name);
-      $user->setPassword($name);
+            // Le nom d'utilisateur et le mot de passe sont identiques pour l'instant
+            $user->setUsername($name);
+            $user->setPassword($name);
 
-      // On ne se sert pas du sel pour l'instant
-      $user->setSalt('');
-      // On définit uniquement le role ROLE_USER qui est le role de base
-      $user->setRoles(array('ROLE_USER'));
+            // On ne se sert pas du sel pour l'instant
+            $user->setSalt('');
+            // On définit uniquement le role ROLE_USER qui est le role de base
+            $user->setRoles(array('ROLE_USER'));
+            $user->setEmail($name.'@gmail.test.com');
 
-      // On le persiste
-      $manager->persist($user);
+            // On le persiste
+            $manager->persist($user);
+        }
+        $user = new User;
+        $user->setUsername('Benoit');
+        $user->setPassword('benoit');
+        $user->setSalt('');
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setEmail('bntdbs@gmail.com');
+        $manager->persist($user);
+
+
+        // On déclenche l'enregistrement
+        $manager->flush();
     }
-
-    // On déclenche l'enregistrement
-    $manager->flush();
-  }
 }
